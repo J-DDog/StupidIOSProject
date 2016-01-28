edf93920d03f8b70058456e3aff3b959b5e06055
Traitortots <jker3169@740s-pl-546.local>
2016-01-28 07:43:31 -0700

    added random color

--- StupidIOSProject/ViewController.swift
+++ StupidIOSProject/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func changeColor(sender: UIButton)
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+        
 
-    override func viewDidLoad() {
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    
 
 
 }
